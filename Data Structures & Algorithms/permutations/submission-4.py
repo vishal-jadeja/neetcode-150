75ad921f489624cd7def6955f8737f051f5b74e3
Vishal Jadeja <jadejavishal6@gmail.com>
2026-05-10 22:46:37 +0530

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(index):
+            if (index == len(nums)):
+                res.append(nums[:])
+                return
+            for i in range(index, len(nums)):
+                nums[index], nums[i] = nums[i], nums[index]
+                dfs(index + 1)
+                nums[index], nums[i] = nums[i], nums[index]
+        dfs(0)
+        return res
